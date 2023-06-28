9137a8fbb1390d1d4f98a443eb4735a5a210a6ae
alan000322 <alan000322@gmail.com>
2023-06-29 00:09:38 +0800

    update

---
+++ js/animate.js
@@ -0,0 +1,73 @@
+$(window).on('scroll', function () {
+    var scrollElement = $('.topic-1');
+    var windowHeight = $(window).height();
+    var windowScrollTop = $(window).scrollTop();
+    var elementOffsetTop = scrollElement.offset().top;
+
+    if (windowScrollTop + windowHeight >= elementOffsetTop) {
+        scrollElement.addClass('animate__animated animate__fadeIn');
+    } 
+});
+
+$(window).on('scroll', function () {
+    var scrollElement = $('.chapter-1');
+    var windowHeight = $(window).height();
+    var windowScrollTop = $(window).scrollTop();
+    var elementOffsetTop = scrollElement.offset().top;
+
+    if (windowScrollTop + windowHeight >= elementOffsetTop) {
+        scrollElement.addClass('animate__animated animate__fadeIn animate__delay-1s');
+    } 
+});
+$(window).on('scroll', function () {
+    var scrollElement = $('.topic-2');
+    var windowHeight = $(window).height();
+    var windowScrollTop = $(window).scrollTop();
+    var elementOffsetTop = scrollElement.offset().top;
+
+    if (windowScrollTop + windowHeight >= elementOffsetTop) {
+        scrollElement.addClass('animate__animated animate__fadeIn');
+    } 
+});
+
+$(window).on('scroll', function () {
+    var scrollElement = $('.chapter-2');
+    var windowHeight = $(window).height();
+    var windowScrollTop = $(window).scrollTop();
+    var elementOffsetTop = scrollElement.offset().top;
+
+    if (windowScrollTop + windowHeight >= elementOffsetTop) {
+        scrollElement.addClass('animate__animated animate__fadeIn animate__delay-1s');
+    } 
+});
+$(window).on('scroll', function () {
+    var scrollElement = $('.topic-3');
+    var windowHeight = $(window).height();
+    var windowScrollTop = $(window).scrollTop();
+    var elementOffsetTop = scrollElement.offset().top;
+
+    if (windowScrollTop + windowHeight >= elementOffsetTop) {
+        scrollElement.addClass('animate__animated animate__fadeIn');
+    } 
+});
+
+$(window).on('scroll', function () {
+    var scrollElement = $('.chapter-3');
+    var windowHeight = $(window).height();
+    var windowScrollTop = $(window).scrollTop();
+    var elementOffsetTop = scrollElement.offset().top;
+
+    if (windowScrollTop + windowHeight >= elementOffsetTop) {
+        scrollElement.addClass('animate__animated animate__fadeIn animate__delay-1s');
+    } 
+});
+
+
+
+$(document).ready(function() {
+    var guide = $('.guide');
+    setTimeout(function() {
+        guide.addClass('d-none');
+    }, 4000);
+    });
+// Set the timeout
