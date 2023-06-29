f963416c8094b60993d70026589b57691c1195a4
alan000322 <alan000322@gmail.com>
2023-06-29 17:05:36 +0800

    update

--- js/animate.js
+++ js/animate.js
@@ -50,6 +50,56 @@ $(window).on('scroll', function () {
         scrollElement.addClass('animate__animated animate__fadeIn');
     } 
 });
+$(window).on('scroll', function () {
+    var scrollElement = $('.solution-card-5');
+    var windowHeight = $(window).height();
+    var windowScrollTop = $(window).scrollTop();
+    var elementOffsetTop = scrollElement.offset().top;
+
+    if (windowScrollTop + windowHeight >= elementOffsetTop) {
+        scrollElement.addClass('animate__animated animate__fadeIn');
+    } 
+});
+$(window).on('scroll', function () {
+    var scrollElement = $('.solution-card-4');
+    var windowHeight = $(window).height();
+    var windowScrollTop = $(window).scrollTop();
+    var elementOffsetTop = scrollElement.offset().top;
+
+    if (windowScrollTop + windowHeight >= elementOffsetTop) {
+        scrollElement.addClass('animate__animated animate__fadeIn');
+    } 
+});
+$(window).on('scroll', function () {
+    var scrollElement = $('.solution-card-1');
+    var windowHeight = $(window).height();
+    var windowScrollTop = $(window).scrollTop();
+    var elementOffsetTop = scrollElement.offset().top;
+
+    if (windowScrollTop + windowHeight >= elementOffsetTop) {
+        scrollElement.addClass('animate__animated animate__fadeIn');
+    } 
+});
+$(window).on('scroll', function () {
+    var scrollElement = $('.solution-card-2');
+    var windowHeight = $(window).height();
+    var windowScrollTop = $(window).scrollTop();
+    var elementOffsetTop = scrollElement.offset().top;
+
+    if (windowScrollTop + windowHeight >= elementOffsetTop) {
+        scrollElement.addClass('animate__animated animate__fadeIn');
+    } 
+});
+$(window).on('scroll', function () {
+    var scrollElement = $('.solution-card-3');
+    var windowHeight = $(window).height();
+    var windowScrollTop = $(window).scrollTop();
+    var elementOffsetTop = scrollElement.offset().top;
+
+    if (windowScrollTop + windowHeight >= elementOffsetTop) {
+        scrollElement.addClass('animate__animated animate__fadeIn');
+    } 
+});
 
 $(window).on('scroll', function () {
     var scrollElement = $('.chapter-3');
