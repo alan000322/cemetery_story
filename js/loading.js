15326f31f6f7f73a3f403a22898841d84c000755
alan000322 <alan000322@gmail.com>
2023-06-29 20:25:33 +0800

    update

---
+++ js/loading.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+        var loadingPage = document.getElementById('loading-page');
+    
+        setTimeout(function () {
+            loadingPage.style.display = 'none';
+            loadingPage.addClass('animate__animated animate__fadeOut');
+        }, 1000); // 3000 milliseconds = 3 seconds
+})
+// Set the timeout
+
+// function hideLoadingPage() {
+//     var loadingPage = document.getElementById('loading-page');
+
+//     setTimeout(function () {
+//         loadingPage.style.display = 'none';
+//     }, 1000); // 3000 milliseconds = 3 seconds
+// }
+
