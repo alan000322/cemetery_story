68d036331fcf1328a2dd43ea0ea2d5cc1bde0acf
alan000322 <alan000322@gmail.com>
2023-06-30 02:04:05 +0800

    update

--- js/loading.js
+++ js/loading.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         setTimeout(function () {
             loadingPage.style.display = 'none';
             loadingPage.addClass('animate__animated animate__fadeOut');
-        }, 1000); // 3000 milliseconds = 3 seconds
+        }, 5000); // 3000 milliseconds = 3 seconds
 })
 // Set the timeout
 
