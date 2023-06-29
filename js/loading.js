0452e9b1203da8f22ca4b9995cde287801ce6d6e
alan000322 <alan000322@gmail.com>
2023-06-30 03:07:16 +0800

    update loading ornot

--- js/loading.js
+++ js/loading.js
@@ -1,11 +1,13 @@
-$(document).ready(function() {
-        var loadingPage = document.getElementById('loading-page');
+// $(document).ready(function() {
     
-        setTimeout(function () {
-            loadingPage.style.display = 'none';
-            loadingPage.addClass('animate__animated animate__fadeOut');
-        }, 5000); // 3000 milliseconds = 3 seconds
-})
+//         setTimeout(function () {
+//             // var loadingPage = document.getElementById('loading-page');
+//             var loadingPage = $('#loading-page');
+//             loadingPage.addClass('animate__animated animate__fadeOut');
+//             loadingPage.addClass('others_none');
+//             loadingPage.style.display = 'none';
+//         }, 5000); // 3000 milliseconds = 3 seconds
+// })
 // Set the timeout
 
 // function hideLoadingPage() {
---
+++ js/loading_.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    
+        setTimeout(function () {
+            var loadingPage = document.getElementById('loading-page');
+            // var loadingPage = $('#loading-page');
+            // loadingPage.addClass('animate__animated animate__fadeOut');
+            // loadingPage.addClass('others_none');
+            loadingPage.style.display = 'none';
+        }, 5000); // 3000 milliseconds = 3 seconds
+})
+// Set the timeout
+
+// function hideLoadingPage() {
+//     var loadingPage = document.getElementById('loading-page');
+
+//     setTimeout(function () {
+//         loadingPage.style.display = 'none';
+//     }, 1000); // 3000 milliseconds = 3 seconds
+// }
+
